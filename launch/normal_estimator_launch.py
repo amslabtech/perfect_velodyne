4d939309885f046918b2ca201cb3de92f7f8ca3d
Kazuki Takahashi <tkhsh.schoen@gmail.com>
2020-07-06 11:58:04 +0900

    update launch for supporting foxy

--- launch/normal_estimator_launch.py
+++ launch/normal_estimator_launch.py
@@ -1,17 +1,19 @@
 import os
-from launch import LaunchDescription
-from launch_ros.actions import Node
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
 
 config_path = os.path.join(os.path.dirname(__file__), '../config')
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
             package='perfect_velodyne',
             node_namespace='',
-            node_executable='normal_estimator_node',
-            node_name='normal_estimator',
+            executable='normal_estimator_node',
+            name='normal_estimator',
             parameters=[os.path.join(config_path, 'param.yaml')],
             output='screen'
         )
